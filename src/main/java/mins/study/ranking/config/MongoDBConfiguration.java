f2ad932a137f93cb1d862e3c9de9654945c2c209
Mingsicogi <codedoctor119@gmail.com>
2020-08-11 01:39:54 +0900

    + add redis rank function implement

--- src/main/java/mins/study/ranking/config/MongoDBConfiguration.java
+++ src/main/java/mins/study/ranking/config/MongoDBConfiguration.java
@@ -1,7 +1,9 @@
 package mins.study.ranking.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@Configuration
 @EnableMongoRepositories
 public class MongoDBConfiguration {
 
